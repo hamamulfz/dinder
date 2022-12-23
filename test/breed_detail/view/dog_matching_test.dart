c91e5a539323d441509e5e215b9dd6f0b6c42bb4
Hamamul Fauzi <hamamul.fauzi@gmail.com>
2022-12-23 09:08:52 +0700

    clean up

--- test/breed_detail/view/dog_matching_test.dart
+++ test/breed_detail/view/dog_matching_test.dart
@@ -1,6 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:dinder/pages/breed_detail/breed_detail.dart';
-import 'package:dinder/pages/breed_detail/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
